efd724652123894978b99af14e2866e7bbddc5da
Jom4nJi <lukjan1999@gmail.com>
2016-08-25 20:26:50 +0200

    Initial Commit

---
+++ ALI-SchemaTests/ALI_SchemaTests.swift
@@ -0,0 +1,36 @@
+//
+//  ALI_SchemaTests.swift
+//  ALI-SchemaTests
+//
+//  Created by Luka Janković on 25/08/16.
+//  Copyright © 2016 ALI-Team. All rights reserved.
+//
+
+import XCTest
+@testable import ALI_Schema
+
+class ALI_SchemaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
